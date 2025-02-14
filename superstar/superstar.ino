faf6920c58e4eb2f39c399a0b582d05bc5c5e67b
Jan Schumacher Vincke <janschumacher98@gmail.com>
2025-02-14 15:06:30 +0100

    Improvement of the clarity and logic of the code

--- superstar/superstar.ino
+++ superstar/superstar.ino
@@ -1,33 +1,35 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 
-
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_DCMotor *motorLeft = AFMS.getMotor(1);
 Adafruit_DCMotor *motorRight = AFMS.getMotor(2);
 
-// #define CPT_LEFT 7       // Broche pour le capteur gauche
-// #define CPT_RIGHT 8     // Broche pour le capteur droit
-
-// #define CPT_US_TRIG_PIN 12 // Broche TRIG pour le capteur ultrason
-// #define CPT_US_ECHO_PIN 13 // Broche ECHO pour le capteur ultrason
-
+// ================================================================
+//                           Connection
+// ================================================================
 #define CPT_LEFT 7
 #define CPT_RIGHT 8
 #define CPT_US_TRIG_PIN 12
 #define CPT_US_ECHO_PIN 9
 
+// ================================================================
+//                           Parameters
+// ================================================================
+
 volatile int speed = 65;
 
 float distance = 0.0;
-const float seuil = 5.0;
-bool isSurfaceBelow = false;
+const float threshold = 5.0;
+bool isSurfaceBelow = true;
 
 volatile int counter = 1;
-const int max_int = 0;
+const int maxInt = 0;
 
 
-
+// ================================================================
+//                           Setup
+// ================================================================
 void setup() {
   delay(5000);
   pinMode(CPT_LEFT, INPUT);
@@ -37,12 +39,12 @@ void setup() {
   pinMode(CPT_US_ECHO_PIN, INPUT);
 
   AFMS.begin();
-
-  
 }
 
+// ================================================================
+//                           Loop
+// ================================================================
 void loop() {
-  
   digitalWrite(CPT_US_TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(CPT_US_TRIG_PIN, HIGH);
@@ -52,27 +54,23 @@ void loop() {
   long duration = pulseIn(CPT_US_ECHO_PIN, HIGH,10000);
   distance = (duration * 0.034) / 2; 
 
-  
+  // Check if this is a good solution.
   counter = counter + 1;
   if (counter >= 600){
     speed = 40;
   }
 
-  
-  if (distance >= seuil) {
-    isSurfaceBelow = true;  // Vide détecté
+  if (distance >= threshold) {
+    isSurfaceBelow = false; 
     motorLeft->run(RELEASE);
     motorRight->run(RELEASE);
-
-    
     return;     
   } else {
-    isSurfaceBelow = false; // Pas de vide
+    isSurfaceBelow = true; 
   }
 
-  
-  bool left = digitalRead(CPT_LEFT);   // Capteur gauche
-  bool right = digitalRead(CPT_RIGHT); // Capteur droit
+  bool left = digitalRead(CPT_LEFT);   
+  bool right = digitalRead(CPT_RIGHT);
 
   if (!left && !right) {
     motorLeft->setSpeed(speed);
