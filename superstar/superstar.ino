26ffa55a8e3e9e1f592d90fa93008b6179f1e81f
Jan Schumacher Vincke <janschumacher98@gmail.com>
2025-02-20 10:07:12 +0100

    Code non fonctionnel

--- superstar/superstar.ino
+++ superstar/superstar.ino
@@ -8,16 +8,16 @@ Adafruit_DCMotor *motorRight = AFMS.getMotor(2);
 // ================================================================
 //                           Connection
 // ================================================================
-#define CPT_LEFT 7
-#define CPT_RIGHT 8
-#define CPT_US_TRIG_PIN 12
+#define CPT_LEFT 8
+#define CPT_RIGHT 7
+#define CPT_US_TRIG_PIN 10
 #define CPT_US_ECHO_PIN 9
 
 // ================================================================
 //                           Parameters
 // ================================================================
 
-volatile int speed = 65;
+volatile int speed = 255; // base speed 65
 
 float distance = 0.0;
 const float threshold = 5.0;
@@ -47,58 +47,54 @@ void setup() {
 // ================================================================
 void loop() {
   digitalWrite(CPT_US_TRIG_PIN, LOW);
-  delayMicroseconds(2);
+  delayMicroseconds(5); // verifier la valeur 
   digitalWrite(CPT_US_TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(CPT_US_TRIG_PIN, LOW);
 
-  long duration = pulseIn(CPT_US_ECHO_PIN, HIGH,10000);
-  distance = (duration * 0.034) / 2; 
+  long duration = pulseIn(CPT_US_ECHO_PIN, HIGH, 30000);
+  distance = (duration * .0343) / 2; 
 
   // Check if this is a good solution.
-  counter = counter + 1;
-  if (counter >= 600){
-    speed = 40;
-  }
+  // counter = counter + 1;
+  // if (counter >= 600){
+  //   speed = 40;
+  // }
 
   if (distance >= threshold) {
     isSurfaceBelow = false; 
     motorLeft->run(RELEASE);
     motorRight->run(RELEASE);
+    Serial.println(String(distance) + ": halte");
     return;     
   } else {
+    Serial.println(distance);
     isSurfaceBelow = true;
   }
 
   bool left = digitalRead(CPT_LEFT);   
   bool right = digitalRead(CPT_RIGHT);
 
-  if(!left){
-    Serial.println("left");
-  }
-  if(!right){
-    Serial.println("right");
-  }
-
-  if (!left && !right) {
-    motorLeft->setSpeed(speed);
-    motorRight->setSpeed(speed);
-    motorLeft->run(FORWARD);
-    motorRight->run(FORWARD);
-  } else if (!left && right) {
+  if (!left && right) {
     motorLeft->run(RELEASE);
     motorRight->setSpeed(speed);
     motorRight->run(FORWARD);
+    Serial.println("Left");
+    // des que qu'on rentre dans cette condition le capteur US ne fonctionne plus comme il devrait
   } else if (left && !right) {
     motorRight->run(RELEASE);
     motorLeft->setSpeed(speed);
     motorLeft->run(FORWARD);
+    Serial.println("Right");
   } else {
     motorLeft->setSpeed(speed);
     motorRight->setSpeed(speed);
     motorLeft->run(FORWARD);
     motorRight->run(FORWARD);
+    Serial.println("Forward");
   }
   //Serial.println(speed);
-  delay(10);
+  delay(1000);
 }
+
+// 
